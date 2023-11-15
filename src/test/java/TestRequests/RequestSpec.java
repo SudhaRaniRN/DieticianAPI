0b1bed478b917828ea3952b061bd142cbba4e4f6
Sudha Rani <mailsudhamaha@gmail.com>
2023-11-14 23:40:40 -0800

    The Framework push

---
+++ src/test/java/TestRequests/RequestSpec.java
@@ -0,0 +1,5 @@
+package TestRequests;
+
+public class RequestSpec {
+
+}
---
+++ src/test/java/TestRunner/TestRunner.java
@@ -0,0 +1,28 @@
+package TestRunner;
+
+
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin= {"pretty", "html:target/cucumberreport.html"},
+				monochrome = true,
+				tags = "",
+				features= {"src/test/resources/features"},
+				glue = {"StepDefinitions"}
+		
+		
+		
+		
+		)
+
+
+
+public class TestRunner {
+
+}
+
