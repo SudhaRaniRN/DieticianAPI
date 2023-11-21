7aacb0c4ae3e1dffde14a31aff58bed88e90689d
Sudha Rani Rama naik <133553553+SudhaRaniRN@users.noreply.github.com>
2023-11-21 14:42:02 -0800

    Update RequestSpec.java

--- src/test/java/TestRequests/RequestSpec.java
+++ src/test/java/TestRequests/RequestSpec.java
@@ -76,19 +76,84 @@ public class RequestSpec {
 		RequestSpecification res = req.build();
 		return res;
 	}
-	public RequestSpecification GetAllMorbidity() {
+	public static RequestSpecification GetAllMorbidity() {
 		req.setBaseUri(URLs.BaseURL);
 		req.setBasePath(URLs.GetAllMorbidity);
 		RequestSpecification res = req.build();
 		return res;
 	}
 	
-	public RequestSpecification GetMorbidityByName() {
+	public static RequestSpecification WrongendpointGetAllMorbidity() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.NegativeEndpointGetAllMorbidity);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification FastingGlucose() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName1);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification AverageGlucose() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName2);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification PlasmaGlucose() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName3);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification HbA1c() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName4);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification TSH() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName5);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification T3() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName6);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification T4() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName7);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification BloodPressureLevels() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.MorbidityByName8);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	public static RequestSpecification InvalidMorbidityName() {
+		req.setBaseUri(URLs.BaseURL);
+		req.setBasePath(URLs.NegativeMorbidityByName);
+		RequestSpecification res = req.build();
+		return res;
+	}
+	
+	
+	
+	/*public RequestSpecification GetMorbidityByName() {
 		req.setBaseUri(URLs.BaseURL);
 		req.setBasePath(URLs.GetMorbidityByName);
 		RequestSpecification res = req.build();
 		return res;
-	}
+	}*/
+	
+}
 	
 	
 	
